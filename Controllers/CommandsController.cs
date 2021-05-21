efccf205a3e682681f0525789de23890eb05596c
phher802 <pher024@gmail.com>
2021-05-20 21:59:25 -0500

    Add instance of MockCommanderRepo to controller and create variables in GET methods

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Commander.Controllers
@@ -8,19 +10,23 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandController : ControllerBase
     {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
 
         //GET api/commands
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands()
         {
-
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+            //use ok to return ok 200 
         }
 
         //GET api/commands/{id}
         [HttpGet("{id}")]
         public ActionResult<CommandController> GetCommandbyId(int id)
         {
-
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
 
         }
     }
